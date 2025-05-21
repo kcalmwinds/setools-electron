c9693b7b2fb0000d0e966d2ed56598e0a424c5b8
Mark Cameron <mark.cameron@microsoft.com>
2025-05-21 00:37:39 -0500

    v1.0.5

--- forge.config.js
+++ forge.config.js
@@ -40,25 +40,25 @@ module.exports = {
       name: '@electron-forge/publisher-github',
       config: {
         repository: {
-          owner: 'delimitertech',
-          name: 'electron-starter'
+          owner: 'kcalmwinds',
+          name: 'setools-electron'
         },
         prerelease: true
       }
     }
   ],
+
+
+  // Fuses are used to enable/disable various Electron functionality
+  // at package time, before code signing the application
+  new FusesPlugin({
+    version: FuseVersion.V1,
+    [FuseV1Options.RunAsNode]: false,
+    [FuseV1Options.EnableCookieEncryption]: true,
+    [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+    [FuseV1Options.EnableNodeCliInspectArguments]: false,
+    [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+    [FuseV1Options.OnlyLoadAppFromAsar]: true,
+  })
   
-    
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-  //   new FusesPlugin({
-  //     version: FuseVersion.V1,
-  //     [FuseV1Options.RunAsNode]: false,
-  //     [FuseV1Options.EnableCookieEncryption]: true,
-  //     [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-  //     [FuseV1Options.EnableNodeCliInspectArguments]: false,
-  //     [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-  //     [FuseV1Options.OnlyLoadAppFromAsar]: true,
-  //   })
-  // ],
 }
